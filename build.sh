5e61692ee56711b4c78d95375ba4bbb58b24d62f
Christian Banse <christian.banse@aisec.fraunhofer.de>
2025-01-08 15:23:28 +0100

    Building on Linux arm64 (#2)

--- build.sh
+++ build.sh
@@ -1,16 +1,31 @@
 #!/bin/bash
-ARCH=`uname -s | tr '[:upper:]' '[:lower:]'`
+OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+ARCH=`arch`
 
-if [ $ARCH == "darwin" ]
+if [ $OS == "darwin" ]
 then
     EXTENSION="dylib"
 else
     EXTENSION="so"
 fi
 
-CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o libgoast-amd64.${EXTENSION} lib.go
+echo "Building on $OS for $ARCH"
 
-if [ $ARCH == "darwin" ]
+if [ $OS == "darwin" ]
 then
     CGO_ENABLED=1 GOARCH=arm64 go build -buildmode=c-shared -o libgoast-arm64.${EXTENSION} lib.go
+    CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o libgoast-amd64.${EXTENSION} lib.go
+fi
+
+if [ $OS == "linux" ]
+then
+    if [ $ARCH == "aarch64" ]
+    then
+        CGO_ENABLED=1 GOARCH=arm64 go build -buildmode=c-shared -o libgoast-arm64.${EXTENSION} lib.go
+    fi
+
+    if [ $ARCH == "x86_64" ]
+    then
+        CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o libgoast-amd64.${EXTENSION} lib.go
+    fi
 fi
