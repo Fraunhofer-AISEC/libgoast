f4c7cc4dcd92ddb6703e6354d35edfc5100d7ebf
Christian Banse <oxisto@clouditor.io>
2025-01-08 13:50:50 +0100

    Building on Linux arm64

--- build.sh
+++ build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
-ARCH=`uname -s | tr '[:upper:]' '[:lower:]'`
+OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+ARCH=`arch`
 
-if [ $ARCH == "darwin" ]
+if [ $OS == "darwin" ]
 then
     EXTENSION="dylib"
 else
     EXTENSION="so"
 fi
 
-CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o libgoast-amd64.${EXTENSION} lib.go
-
-if [ $ARCH == "darwin" ]
+if [ $OS == "darwin" ]
 then
     CGO_ENABLED=1 GOARCH=arm64 go build -buildmode=c-shared -o libgoast-arm64.${EXTENSION} lib.go
+    CGO_ENABLED=1 GOARCH=amd64 go build -buildmode=c-shared -o libgoast-amd64.${EXTENSION} lib.go
+else
+    CGO_ENABLED=1 GOARCH=$ARCH go build -buildmode=c-shared -o libgoast-amd64.${EXTENSION} lib.go
 fi
